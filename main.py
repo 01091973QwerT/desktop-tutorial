bb67dd520642634cca349a402491da6a7948038f
01091973QwerT <fastovskiyps4@mail.ru>
2025-01-16 02:51:54 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-#uvicorn main:app --reload    Актвация
 from fastapi import FastAPI
+from app.routers import task
+from app.routers import users
 
 app = FastAPI()
 
-
-
-@app.get("/user/admin")
-async def admin() -> dict:
-    return {"message": "Вы вошли как администратор"}
-
-@app.get("/user/{user_id}")
-async def user_id(user_id: int) -> dict: # user_id автоматически преобразуется в целое число
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
-
-@app.get("/user")
-async def user_info(username: str, age: int) -> dict: #username и age берутся из query params
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
-
 @app.get("/")
-async def root() -> dict:
-    return {"message": "Главная страница"}
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
 
+app.include_router(task.router)
+app.include_router(users.router)
